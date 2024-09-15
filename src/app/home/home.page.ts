4160a4d4354729837227849e8e0fbbfb0b766b33
Lucas Martins <lucas.rodriguesmartins@hotmail.com>
2024-09-15 15:49:00 -0300

    Criação do projeto Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,30 @@ export class HomePage {
 
   constructor() {}
 
+  public precoAlcool = "";
+  public precoGasolina = "";
+  public resultado: String = "Resultado";
+
+  public calcular() {
+    //Validar se os campos foram preenchidos
+    if (this.precoAlcool && this.precoGasolina) {
+      
+      var pAlcool = parseFloat(this.precoAlcool);
+      var pGasolina = parseFloat(this.precoGasolina);
+
+      var res = pAlcool / pGasolina
+      if (res>= 0.7) {
+        this.resultado = "Melhor utilizar Gasolina"
+      }else{
+        this.resultado = "Melhor utilizar o Alcool"
+      }
+
+    } else {
+      this.resultado = "Preencha corretamente os campos!";
+    }
+  }
+  limparCampos() {
+      this.precoAlcool = "";
+      this.precoGasolina = "";
+  }
 }
